6ef601ecc18b19fefc08f3a077d19c2519033a51
Tiarawr <yumomik@gmail.com>
2025-05-27 04:05:23 +0700

    test

--- frontend/src/AdminDashboard.jsx
+++ frontend/src/AdminDashboard.jsx
@@ -73,7 +73,7 @@ export default function AdminDashboard() {
       setLoading(true);
       setError(null);
       const response = await axios.get(
-        `${import.meta.env.VITE_API_URL}/admin/orders-with-shoes`
+        fetch(`${import.meta.env.VITE_API_URL}/api/admin/orders-with-shoes`)
       );
 
       const ordersWithParsedImages = response.data.map((order) => ({
